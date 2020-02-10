c1066a1e8ced41885676cfe24cb2a5a9255719c3
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2020-02-10 22:12:30 +0700

    Create standart directory

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
